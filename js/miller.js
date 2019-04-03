4cf4a3594e8e9b91bc6fae8384bf814996076391
mberhe <mberhe@apposit.com>
2019-04-03 17:09:15 +0300

    added miller column title update icon on hover

--- js/miller.js
+++ js/miller.js
@@ -124,7 +124,7 @@ function CategoryItem() {
         _this.numChildren = numChildren;
         _this.setHasChildren(numChildren != 0);
     }
-    
+
     _this.getNumChildren = function(){
         return _this.numChildren;
     };
@@ -266,7 +266,9 @@ function guid() {
                     millerColTitleText = $("<div>").addClass("miller-col-title-text").append($("<span/>").text(category.categoryName));
 
                 if (!readOnly) {
-                    var millerColAction = $("<span/>").addClass("miller-col-actions").append($("<i/>").addClass("material-icons").addClass("action-add").text("add"));
+                    var millerColAction = $("<span/>").addClass("miller-col-actions");
+                    millerColAction.append($("<i/>").addClass("material-icons").addClass("action-edit").addClass("miller-col-title-icons").text("edit"));
+                    millerColAction.append($("<i/>").addClass("material-icons").addClass("action-add").addClass("miller-col-title-icons").text("add"));
                     millerColTitleText.append(millerColAction);
                 }
 
@@ -362,7 +364,7 @@ function guid() {
                 listDeletedItem.remove();
 
             return this;
-            
+
         }else if ("destroy" == args[0]) {
 
             if (isInitialized.call(this)) {
@@ -426,9 +428,9 @@ function guid() {
             }
 
             if(item.numChildren != null && item.numChildren != 0){
-                
+
                 millerColListItem.append($("<span/>").addClass("num-children-badge").text(item.numChildren));
-                
+
             }
 
             return millerColListItem;
@@ -935,7 +937,7 @@ function guid() {
 
                  var children = currentColContainer.data("children");
 
-                 console.log("children:" + children)  
+                 console.log("children:" + children)
 
                  if(children){
 
@@ -943,7 +945,7 @@ function guid() {
 
                         if(data.itemId === children[i].itemId){
 
-                            if(children[i].childCategory){ 
+                            if(children[i].childCategory){
 
                                 $(millerColumn).millerColumn("addCol", children[i].childCategory);
 
@@ -954,7 +956,7 @@ function guid() {
 
                     }
 
-                 } 
+                 }
 
                 if (isDebugEnabled) {
                     console.log("fired item-selected event: " + JSON.stringify(data))
@@ -1015,6 +1017,25 @@ function guid() {
 
             });
 
+            getMillerColsBody.call(millerColumn).on("click", ".miller-col-actions .action-edit", function () {
+
+                var currentColContainer = $(this).closest(getColContainerSelector());
+                var parentColContainer = currentColContainer.prev();
+
+                //Firing add-item event.
+                var data = getCategory.call(currentColContainer);
+
+                if (parentColContainer)
+                    data.parentId = $(parentColContainer).find(getColListItemSelector()).filter(SELECTOR_IS_SELECTED).data("item-id");
+
+                $(currentColContainer).trigger("add-item", data);
+
+                if (isDebugEnabled) {
+                    console.log("fired add item event: " + JSON.stringify(data));
+                }
+
+            });
+
 
             $(window).on("resize." + $(millerColumn).attr("id"), function (event) {
 
@@ -1022,7 +1043,7 @@ function guid() {
 
                     if (!isInitialized.call(millerColumn))
                         return;
-                    
+
                     if (isDebugEnabled) {
                         console.log("window resized..");
                     }
